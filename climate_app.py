21d25e0cefdb815223aeddfca6dfe4b20a82a89e
SommerKim <74261427+SommerKim@users.noreply.github.com>
2021-03-03 13:40:22 -0500

    Added first three web pages

---
+++ climate_app.py
@@ -0,0 +1,89 @@
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///hawaii-Copy1.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+#################################################
+# Flask Routes
+#################################################
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start> and /api/v1.0/<start>/<end>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return date and precipitation values"""
+    # Query date and precipitation
+    precip = session.query(Measurement.date, Measurement.prcp).all()
+
+    session.close()
+
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return stations"""
+    # Query station names
+    stations = session.query(Station.station, Station.name).all()
+
+    session.close()
+
+    return jsonify(stations)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
